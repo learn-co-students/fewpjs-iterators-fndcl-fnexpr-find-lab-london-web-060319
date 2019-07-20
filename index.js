7f429be91290eab560b9f25c925bce878f809534
Federica Macchi <federica.macchi3@gmail.com>
2019-07-20 14:50:14 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const hasWon = array.find(match => match.result === "W")
+  if (hasWon) {
+    return hasWon.year
+  }
+}
