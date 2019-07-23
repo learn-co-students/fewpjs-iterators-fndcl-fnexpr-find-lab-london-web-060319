bde7f049eee6cb5b49b8838460b47b3389ccd9b5
Pascal Rassaby <pasbynumbers@gmail.com>
2019-07-23 11:20:43 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(game){
+  const winner= game.find(record=>record.result==="W");
+  return (winner ? winner.year :undefined);
+}
